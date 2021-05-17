3a1050d67febaf3dbf598e262d21b2e465a3ff73
esquirenet <84340138+esquirenet@users.noreply.github.com>
2021-05-17 20:05:36 +0200

    Update module.php

--- IPSmorgenwirdes/module.php
+++ IPSmorgenwirdes/module.php
@@ -1,4 +1,31 @@
 <?php
+    // Klassendefinition
+    class IPSmorgenwirdes extends IPSModule {
+ 
+        // Überschreibt die interne IPS_Create($id) Funktion
+        public function Create() {
+            // Diese Zeile nicht löschen.
+            parent::Create();
+ 
+        }
+ 
+        // Überschreibt die intere IPS_ApplyChanges($id) Funktion
+        public function ApplyChanges() {
+            // Diese Zeile nicht löschen
+            parent::ApplyChanges();
+        }
+ 
+        /**
+        * Die folgenden Funktionen stehen automatisch zur Verfügung, wenn das Modul über die "Module Control" eingefügt wurden.
+        * Die Funktionen werden, mit dem selbst eingerichteten Prefix, in PHP und JSON-RPC wiefolgt zur Verfügung gestellt:
+        *
+        * ABC_MeineErsteEigeneFunktion($id);
+        *
+        */
+        public function MeineErsteEigeneFunktion() {
+            // Selbsterstellter Code
+        }
+    }
 
 // Regenradar - Modul zum zyklischen Pollen vom morgenwirdes.de-Niederschlagsradar
 // Quelle: Deutscher Wetterdienst          IPS-Modul v1.0 Build 1000 von ika
